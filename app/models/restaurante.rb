a8173595069174c9fe60d01761f4a125d3f8d61b
FilipeG920 <genesisfilipe11@gmail.com>
2025-09-22 00:38:17 -0300

    validação para registros de restaurante

--- app/models/restaurante.rb
+++ app/models/restaurante.rb
@@ -1,2 +1,8 @@
 class Restaurante < ApplicationRecord
+  validates :nome, :endereco, :especialidade, presence: true
+
+  validates :nome, uniqueness: {
+    scope: :endereco,
+    message: "já existe um restaurante com esse nome neste endereço"
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_16_030409) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_22_025853) do
+  create_table "clientes", force: :cascade do |t|
+    t.string "nome", limit: 80
+    t.integer "idade"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "pessoas", force: :cascade do |t|
     t.string "nome", limit: 80
     t.date "data_de_nascimento"
